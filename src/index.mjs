f020a9d31a47036c2485915448bfd3aa1006c32d
alexandre-fernandez <alexandre-fernandez@gmx.com>
2022-11-12 20:43:56 +0100

    added support for modules

---
+++ src/index.mjs
@@ -0,0 +1,18 @@
+import esbuild from "esbuild"
+import nodeEval from "node-eval"
+
+export default async function getFileExports(path) {
+	if (typeof path !== "string") {
+		throw new Error("`path` must be of type `string`.")
+	}
+	const output = await esbuild.build({
+		entryPoints: [path],
+		bundle: true,
+		format: "cjs",
+		platform: "node",
+		write: false,
+	})
+	const bundle = new TextDecoder().decode(output.outputFiles[0]?.contents)
+	if (!bundle) return {}
+	return nodeEval(bundle, path.replace(/.[a-zA-Z]{2,3}$/, ".cjs"))
+}
